38ed32ca8e992f60800a9058937dd7ada7327f5e
Petrov7 <ppetrov7@protonmail.com>
2020-08-17 12:32:16 +0300

    Solved Problem

--- LeetCode Problems/05 Longest Common Prefix/script.js
+++ LeetCode Problems/05 Longest Common Prefix/script.js
@@ -1,15 +1,15 @@
 function longestCommonPrefix(strs) {
     const len = strs.length;
-  if (len === 0) return "";
+    if (len === 0) return "";
 
-  let prefix = strs[0];
-  for (let i = 1; i < len; i++) {
-    while (strs[i].indexOf(prefix) !== 0) {
-      prefix = prefix.substring(0, prefix.length - 1);
-      if (prefix === "") return "";
+    let prefix = strs[0];
+    for (let i = 1; i < len; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return "";
+        }
     }
-  }
-  
+
   return prefix;
 }
 
---
+++ LeetCode Problems/06 Valid Parentheses/script.js
@@ -0,0 +1,25 @@
+function isValid(input) {
+    const obj = {
+        "(": ")",
+        "{": "}",
+        "[": "]",
+    }
+
+    const stack = [];
+
+    for (const paran of input) {
+        if (obj.hasOwnProperty(paran)) {
+            stack.push(paran)
+        } else {
+            const closeParan = stack.pop();
+            if (paran !== obj[closeParan]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+console.log(isValid("[{}]"));
+console.log(isValid("([)]"));
